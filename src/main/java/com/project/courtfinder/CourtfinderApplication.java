bcf699cc3309bad7dfa97cb37a0443ab96c8d487
Goran Roso <goran.roso@kodelab.hr>
2025-10-31 08:40:11 +0100

    Initial backend project setup

---
+++ src/main/java/com/project/courtfinder/CourtfinderApplication.java
@@ -0,0 +1,11 @@
+package com.project.courtfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtfinderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourtfinderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/courtfinder/CourtfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.courtfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
